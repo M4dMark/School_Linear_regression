62a11aae51747660ebc57516a2a44fde6f92f45c
Marek Reznak <reznakmarek@gmail.com>
2023-05-26 20:39:58 +0200

    Finished

--- main.py
+++ main.py
@@ -1,73 +1,64 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+if __name__ == '__main__':
+    data = pd.read_csv('student-mat.csv', sep=';')
 
-data = pd.read_csv('AI/Linear Regression/student-mat.csv', sep=';')
+    input_cols = input('Enter features from the dataset, you want to use to predict by (feature,feature): ').split(',')
+    target_cols = input('Enter the feature you want to predict (feature): ')
+    x = data[input_cols].values
+    y = data[target_cols].values
 
-data = data[['G1', 'G2', 'G3', 'studytime', 'failures', 'absences']]
-predict = 'G3'
-x = np.array(data.drop([predict], 1))
-y = np.array(data[predict])
-y = y.reshape(395, 1)
+    x = (x - np.mean(x, axis=0)) / np.std(x, axis=0)
 
-x = torch.from_numpy(x.values.astype(np.float32))
-y = torch.from_numpy(y.values.astype(np.float32))
+    x = torch.from_numpy(x.astype(np.float32))
+    y = torch.from_numpy(y.astype(np.float32)).unsqueeze(1)
 
-print(x, y)
-n_samples, n_features = x.shape
-input_size = n_features
-output_size = 1
-model = torch.nn.Linear(input_size, output_size)
+    input_size = x.shape[1]
+    hidden_size = 32
+    output_size = y.shape[1]
 
-learning_rate = 0.10
-criterion = torch.nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    model = nn.Sequential(nn.Linear(input_size, hidden_size),
+                        nn.ReLU(),
+                        nn.Linear(hidden_size, output_size)
+                        )
 
-num_epochs = 395
-for epoch in range(num_epochs):
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    num_epochs = 500
+    batch_size = 32
+
+    for epoch in range(num_epochs):
+        for i in range(0, len(x), batch_size):
+            x_batch = x[i:i+batch_size]
+            y_batch = y[i:i+batch_size]
+            y_predicted = model(x_batch)
+            loss = criterion(y_predicted, y_batch)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        if epoch % 50 == 0:
+            print(f'Epoch {epoch}, loss = {loss.item():.4f}')
+            
     y_predicted = model(x)
-    loss = criterion(y_predicted, y[epoch])
-    loss.backward()
-    optimizer.step()
-    optimizer.zero_grad()
+    mse = criterion(y_predicted, y)
+    print('Mean squared error: ', mse.item())
 
-    if (epoch+1) % 10 == 0:
-        print(f'epoch: {epoch + 1}, loss = {loss.item()}')
-predicted = model(x).detach().numpy()
-plt.plot(x, y, 'ro')
-plt.plot(x, predicted, 'b')
-plt.show()
-'''''
+    y_predicted = y_predicted.detach().numpy()
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
-inputs = torch.from_numpy(x_train)
-targets = torch.from_numpy(y_train)
-inputs = inputs.type(torch.float32)
-targets = targets.type(torch.float32)
-
-print(inputs)
-print(targets)
-class LinearRegression(torch.nn.Module):
-    def __init__(self) -> None:
-        super(LinearRegression, self).__init__()
-        self.linear = torch.nn.Linear(5, 1)
+    print('Predicted values:')
+    for i in range(len(y_predicted)):
+        print(f'Sample {i+1}: {round(y_predicted[i][0]):.2f}, Target = {y[i][0]:.2f}')
     
-    def forward(self, x):
-        y_pred = self.linear(x)
-        return y_pred
-    
-model = LinearRegression()
-criterion = torch.nn.MSELoss(size_average=False)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-
-for epoch in range(395):
-    pred_y = model(inputs)
-    loss = criterion(pred_y, targets)
-
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    print(f'epoch {epoch}, loss {loss.item()}')
-'''''
+    plt.scatter(y, y_predicted, alpha=0.5, label="Data Points")
+    plt.xlabel("Target Values")
+    plt.ylabel("Predicted Values")
+    plt.title("Predicted vs Target Values")
+    plt.legend()
+    plt.show()
